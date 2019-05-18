65cd31cef8d3dce42f710a1c0499dfc5496eeba5
Charly POLY <cpoly55@gmail.com>
2019-05-18 22:32:41 +0200

    doc revamp

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: 'Frontier forms',
   typescript: true,
+  menu: [
+    'Frontier forms',
+    'What is Frontier',
+    'Installation',
+    'Getting started',
+    'Frontier UI-Kit',
+    { name: 'API', menu: ['<Frontier>', 'Frontier UI-Kit'] }
+  ],
 }
